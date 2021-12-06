b69d9d8deb6052334deb72e2026ce09f01b91c21
mary830331 <sara830331@gmail.com>
2021-12-06 13:28:28 +0800

    ++

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+#app
+  Top
+  #middle
+    #left
+      Menu
+      Update
+    #right
+      Marquee
+      Content
+  Footer
+</template>
+
+<script>
+import Top from './components/Top.vue'
+import Menu from './components/Menu.vue'
+import Update from './components/Update.vue'
+import Marquee from './components/Marquee.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    Top,
+    Menu,
+    Update,
+    Marquee,
+    Content,
+    Footer
+  }
+}
+</script>
